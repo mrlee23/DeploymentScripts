e3e2631d9afa8d2819ca7a37a6747b1877f01261
DongSoo Lee <dongsoolee8@gmail.com>
2018-03-19 22:08:45 +0900

    Update git setup

---
+++ Setup/README.md
@@ -0,0 +1,5 @@
+# Setup
+
+## git
+
+- [alpine/git](https://hub.docker.com/r/alpine/git/)
--- Setup/git.sh
+++ Setup/git.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Pull docker image
+docker pull alpine/git
+
 # Add git user
 useradd git -G docker -m -s /bin/bash
 cd /home/git
@@ -8,3 +11,6 @@ cd /home/git
 mkdir .ssh
 cp ~/.ssh/authorized_keys .ssh/
 chown -R git:docker .ssh
+
+# Make the git command using docker
+echo "alias git=\"docker run -ti --rm -v $(pwd):/git -v $HOME/.ssh:/root/.ssh alpine/git\"" >> .bash_profile
